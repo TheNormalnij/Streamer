e3728fdc2c88acaf0e2abc7121fb7c82db69df59
Uladzislau Nikalayevich <zarevi4Vlad@gmail.com>
2024-05-09 22:45:11 +0300

    Add building loader class

--- .mta_interfaces.lua
+++ .mta_interfaces.lua
@@ -3,6 +3,8 @@
 
 ---@class Element
 ---@class Object : Element
+---@class Building : Element
+---@class Water : Element
 
 ---@type fun(element: Element): boolean
 destroyElement = nil
@@ -12,6 +14,15 @@ engineLoadCOL = nil
 
 ---@type fun(modelId: number, x: number, y: number, z: number, rx: number, ry: number, rz: number, isLowLOD: boolean): Object
 createObject = nil
+
+---@type fun(modelId: number, x: number, y: number, z: number, rx: number, ry: number, rz: number, interior: number): Building
+createBuilding = nil
+
+---@type fun(x: number, y: number, z: number, x2: number, y2: number, z2: number, x3: number, y3: number, z3: number, shallow: boolean): Water
+createWater = nil
+
+---@type fun(x: number, y: number, z: number, x2: number, y2: number, z2: number, x3: number, y3: number, z3: number,  x4: number, y4: number, z4: number, shallow: boolean): Water
+createWater = nil
 
 ---@type fun(element: Element, int: integer): boolean
 setElementInterior = nil
@@ -28,3 +39,12 @@ restoreWorldModel = nil
 
 ---@type fun(modelID: number, redius: number, x: number, y: number, z: number): boolean
 removeWorldModel = nil
+
+---@type fun(message: string, level?: number): boolean
+outputDebugString = nil
+
+---@type fun(modelId: number): number
+engineGetModelTXDID = nil
+
+---@type fun(state: boolean): boolean
+setOcclusionsEnabled = nil
---
+++ streamers/MtaBuildingStaticStreamer_client.lua
@@ -0,0 +1,94 @@
+
+---@class (exact) MtaBuildingStaticStreamer : IStreamer, IWithDestructor
+---@field private map IObjectPositionDef[]
+---@field private defs IObjectDef[]
+---@field private enabled boolean
+---@field private buildings Building[]
+MtaBuildingStaticStreamer = class()
+
+---@param map IObjectPositionDef[]
+---@param defs IObjectDef[]
+function MtaBuildingStaticStreamer:create( map, defs )
+    self.map = map
+    self.defs = defs
+    self.enabled = false
+    self.buildings = {}
+end
+
+function MtaBuildingStaticStreamer:destroy( )
+    if self.enabled then
+        self:stop()
+    end
+end
+
+function MtaBuildingStaticStreamer:start( )
+    if self.enabled then
+        return false
+    end
+
+    self:createAllBuilding()
+    self.enabled = true
+    return true
+end
+
+function MtaBuildingStaticStreamer:stop( )
+    if not self.enabled then
+        return false
+    end
+
+    self:destroyAllObjects()
+
+    self.enabled = false
+    return true
+end
+
+---@private
+function MtaBuildingStaticStreamer:createAllBuilding( )
+    local map = self.map
+    local defs = self.defs
+
+    local objectData, modelID, def, building, lod
+
+    local loadedBuildings = {}
+    self.buildings = loadedBuildings
+
+    local setLowLODElement = setLowLODElement
+    local createBuilding = createBuilding
+    local setElementCollisionsEnabled = setElementCollisionsEnabled
+
+    for i = 1, #map do
+        objectData = map[i]
+        def = defs[ objectData[2] ]
+        modelID = objectData[2] and def[1] or objectData[1]
+        building = createBuilding( modelID, objectData[4], objectData[5], objectData[6], objectData[7], objectData[8], objectData[9], objectData[3] )
+        if building then
+
+            if def and not def[4] then
+                setElementCollisionsEnabled( building, false )
+            end
+            if objectData[11] then
+                lod = map[ objectData[11] ]
+                if lod then
+                    setLowLODElement( building, lod )
+                end
+            end
+        else
+            outputDebugString( 'Object can not be created ' ..  modelID, 2 )
+        end
+        loadedBuildings[i] = building
+    end
+end
+
+---@private
+function MtaBuildingStaticStreamer:destroyAllObjects( )
+    local destroyElement = destroyElement
+
+    local buildings = self.buildings
+    for i = #buildings, 1, -1 do
+        if buildings[i] then
+            destroyElement(buildings[i])
+        end
+    end
+
+    self.buildings = {}
+end
