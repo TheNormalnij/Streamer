fc3706b6f8373eaab1267c4f0fbfaffe52ecc85a
Uladzislau Nikalayevich <zarevi4Vlad@gmail.com>
2024-07-20 22:45:23 +0300

    Reorder commands

--- loaders/SanAndreasWorldLoader_client.lua
+++ loaders/SanAndreasWorldLoader_client.lua
@@ -23,13 +23,14 @@ function SanAndreasWorldLoader:load()
 end
 
 function SanAndreasWorldLoader:unload()
+    removeAllGameBuildings()
+
     -- Remove dummies
     local removeWorldModel = removeWorldModel
     for _, modelId in pairs(ID_PHYSICAL) do
         removeWorldModel(modelId, 10000, 0, 0, 0)
     end
 
-    removeAllGameBuildings()
     setOcclusionsEnabled( false )
     setWaterLevel( -5000, true, true, true, false )
 
